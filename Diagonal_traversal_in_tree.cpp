f43c0f67e0a9f3fb00214dee2e890497e7de625d
Pavan Kumar <pavankumardontha@gmail.com>
2025-07-01 15:14:42 +0530

    Update Diagonal_traversal_in_tree.cpp

--- Diagonal_traversal_in_tree.cpp
+++ Diagonal_traversal_in_tree.cpp
@@ -1,3 +1,37 @@
+class Solution {
+  public:
+    vector<int> diagonal(Node *root) 
+    {
+        vector<int> ans;
+        if(root==NULL)
+        return ans;
+        queue<Node*> q;
+        q.push(root);
+        Node* temp;
+        Node* current;
+        while(!q.empty())
+        {
+            temp = q.front();
+            q.pop();
+            // add all the right nodes to ans and left nodes into the queue
+            current = temp;
+            while(current!=NULL)
+            {
+                ans.push_back(current->data);
+                // if there is any left node to the current node, we push it into the queue
+                if(current->left != NULL)
+                {
+                    // these left nodes belong to the next diagonal
+                    q.push(current->left);
+                }
+                current = current->right;
+            }
+        }
+        return ans;
+    }
+};
+
+
 vector<int> diagonal(Node *root)
 {
     vector<int> ans;
